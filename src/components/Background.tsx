07a82e99a2cbacf19b8bd58f5b89b431c7efc798
Seranie <delvinlim6414@gmail.com>
2025-07-30 12:53:53 +0800

    added volume slider with necessary event listener

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -3,6 +3,7 @@ import styles from './Background.module.css';
 import Pot from './Pot';
 import Spoon from './Spoon';
 import Audio from './Audio';
+import Volume from './Volume';
 
 export default function Background(){
     const potRef = useRef<SVGPathElement>(null);
@@ -12,6 +13,7 @@ export default function Background(){
 
     return (
         <div className={ styles.background }> 
+            <Volume macaroniRef={ macaroniRef } stirringRef={ stirringRef }/>
             <Spoon potRef={ potRef } macaroniRef={ macaroniRef } stirringRef={ stirringRef }/>
             <Pot potRef={ potRef }/>
             <Audio macaroniRef= { macaroniRef } stirringRef={ stirringRef }/>
