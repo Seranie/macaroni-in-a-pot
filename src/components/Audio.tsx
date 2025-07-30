44eac18243a19ccf59053e9d0af37b7e4e7e9800
Seranie <delvinlim6414@gmail.com>
2025-07-30 19:02:26 +0800

    changed file name of macaroni-in-a-pot.flac

--- src/components/Audio.tsx
+++ src/components/Audio.tsx
@@ -16,7 +16,7 @@ export default function Audio({ macaroniRef, stirringRef }: AudioProps){
     return (
         <>
             <audio loop ref={ macaroniRef }>
-                <source src="../../public/macaroni-in-a-pot.flac" type="audio/flac" />
+                <source src="../../public/macaroni.flac" type="audio/flac" />
             </audio>
             <audio loop ref={ stirringRef }>
                 <source src="../../public/stirring-macaroni-and-cheese.mp3" type="audio/mpeg" />
